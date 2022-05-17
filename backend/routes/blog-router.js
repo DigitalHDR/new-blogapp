1f634b38cc7e3107a64bd8b6fd0510f41870c4dc
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-05-17 16:20:31 -0300

    1:07:38 Fazendo o delete (para deletar um blog)

--- backend/routes/blog-router.js
+++ backend/routes/blog-router.js
@@ -4,7 +4,8 @@ import {
   addBlog,
   getAllBlogs,
   updateBlog,
-  getById
+  getById,
+  deleteBlog
 } from '../controllers/blog-controller'
 const blogRouter = express.Router()
 
@@ -12,5 +13,6 @@ blogRouter.get('/', getAllBlogs)
 blogRouter.post('/add', addBlog)
 blogRouter.put('/update/:id', updateBlog)
 blogRouter.get('/:id', getById)
+blogRouter.delete('/:id', deleteBlog)
 
 export default blogRouter
