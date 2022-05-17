eb9307fc7b5d5bd29989b5faaa981953d23440d5
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-05-17 13:59:07 -0300

    46:00 Autentificação de email e senha

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -52,3 +52,30 @@ export const signup = async (req, res, next) => {
     user
   })
 }
+
+export const login = async (req, res, next) => {
+  const { email, password } = req.body
+
+  let existingUser
+  try {
+    existingUser = await User.findOne({
+      email
+    })
+  } catch (err) {
+    return console.log(err)
+  }
+  if (!existingUser) {
+    return res.status(404).json({
+      message: 'Não foi possível encontrar o usuário por este e-mail!'
+    })
+  }
+
+  const isPasswordCorrect = await bcrypt.compareSync(
+    password,
+    existingUser.password
+  )
+  if (!isPasswordCorrect) {
+    return res.status(400).json({ message: 'Senha incorreta!' })
+  }
+  return res.status(200).json({ message: 'Sucesso no Login!' })
+}
--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -1,11 +1,12 @@
 //backend/routes/user-routes.js
 import express from 'express'
 
-import { getAllUser, signup } from '../controllers/user-controller'
+import { getAllUser, login, signup } from '../controllers/user-controller'
 
 const router = express.Router()
 
 router.get('/', getAllUser)
 router.post('/signup', signup)
+router.post('/login', login)
 
 export default router
