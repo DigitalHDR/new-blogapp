4aebc85ae9db2ef9ea131e043bc559b21182ca43
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-05-17 11:43:40 -0300

    17:17 base do projeto mongodb conectado

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 //backend/app.js
 import express from 'express'
-
+import mongoose from 'mongoose'
 const app = express()
 
-app.use('/api', (req, res, next) => {
-  res.send('hello world')
-})
-
-app.listen(5000)
+mongoose
+  .connect(
+    'mongodb+srv://admin:admin@cluster0.yt9ns.mongodb.net/Blog?retryWrites=true&w=majority'
+  )
+  .then(() => app.listen(5000))
+  .then(() => console.log('conectado ao mongodb porta 5000'))
+  .catch((err) => console.log(err))
