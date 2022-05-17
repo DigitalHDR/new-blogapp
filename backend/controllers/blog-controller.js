5b57c896f4d7f5dd40b271735fc5484d58042692
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-05-17 15:02:06 -0300

    1:01:39 Fazendo a rota de update na rota blog

--- backend/controllers/blog-controller.js
+++ backend/controllers/blog-controller.js
@@ -29,3 +29,21 @@ export const addBlog = async (req, res, next) => {
   }
   return res.status(200).json({ blog })
 }
+
+export const updateBlog = async (req, res, next) => {
+  const { title, description } = req.body
+  const blogId = req.params.id
+  let blog
+  try {
+    blog = await Blog.findByIdAndUpdate(blogId, {
+      title,
+      description
+    })
+  } catch (err) {
+    return console.log(err)
+  }
+  if (!blog) {
+    return res.status(500).json({ message: 'impossível atualizar blog' })
+  }
+  return res.status(200).json({ blog })
+}
--- backend/routes/blog-router.js
+++ backend/routes/blog-router.js
@@ -1,9 +1,14 @@
 //backend/routes/blog-routes.js
 import express from 'express'
-import { addBlog, getAllBlogs } from '../controllers/blog-controller'
+import {
+  addBlog,
+  getAllBlogs,
+  updateBlog
+} from '../controllers/blog-controller'
 const blogRouter = express.Router()
 
 blogRouter.get('/', getAllBlogs)
 blogRouter.post('/add', addBlog)
+blogRouter.put('/update/:id', updateBlog)
 
 export default blogRouter
