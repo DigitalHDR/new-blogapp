a4247f2f18429f1112c3dc286341b09fc271352a
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-05-17 14:23:16 -0300

    Ok 52:08 Fazendo outra rota para api (blog)

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
 //backend/app.js
 import express from 'express'
 import mongoose from 'mongoose'
+import blogRouter from './routes/blog-router'
 import router from './routes/user-routes'
 
 const app = express()
 app.use(express.json())
 app.use('/api/user', router)
+app.use('/api/blog', blogRouter)
 
 mongoose
   .connect(
