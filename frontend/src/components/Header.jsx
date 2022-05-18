af541e37ca999417fb82f8d50cc9f542f9fea923
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-05-18 13:42:25 -0300

    1:51:30 blogs e myblogs

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,6 @@
 //frontend/src/components/header.jsx
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import {
   AppBar,
   Box,
@@ -22,10 +23,14 @@ export const Header = () => {
     >
       <Toolbar>
         <Typography variant="h4">BlogsApp</Typography>
-        <Box display="flex">
-          <Tabs value={value} onChange={(e, val) => setValue(val)}>
-            <Tab label="All Blogs"></Tab>
-            <Tab label="My Blogs"></Tab>
+        <Box display="flex" marginLeft={'auto'} marginRight={'auto'}>
+          <Tabs
+            textColor="inherit"
+            value={value}
+            onChange={(e, val) => setValue(val)}
+          >
+            <Tab LinkComponent={Link} to="/blogs" label="All Blogs"></Tab>
+            <Tab LinkComponent={Link} to="/myBlogs" label="My Blogs"></Tab>
           </Tabs>
         </Box>
 
@@ -43,6 +48,13 @@ export const Header = () => {
             color="secondary"
           >
             Signup
+          </Button>
+          <Button
+            variant="contained"
+            sx={{ margin: 1, borderRadius: 10 }}
+            color="secondary"
+          >
+            Logout
           </Button>
         </Box>
       </Toolbar>
