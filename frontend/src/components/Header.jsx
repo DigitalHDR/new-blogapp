472d51bc87b58b3a6b6fcc328b11268a9e259120
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-05-18 00:01:39 -0300

    header add

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,32 @@
 //frontend/src/components/header.jsx
 import React from 'react'
-import { AppBar, Toolbar, Typography } from '@mui/material'
+import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material'
 
 export const Header = () => {
   return (
-    <AppBar>
+    <AppBar
+      sx={{
+        background: '#6495ed'
+      }}
+    >
       <Toolbar>
-        <Typography>BlogsApp</Typography>
+        <Typography variant="h4">BlogsApp</Typography>
+        <Box display="flex" marginLeft="auto">
+          <Button
+            variant="contained"
+            sx={{ margin: 1, borderRadius: 10 }}
+            color="secondary"
+          >
+            Login
+          </Button>
+          <Button
+            variant="contained"
+            sx={{ margin: 1, borderRadius: 10 }}
+            color="secondary"
+          >
+            Signup
+          </Button>
+        </Box>
       </Toolbar>
     </AppBar>
   )
