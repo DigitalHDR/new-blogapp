a9e964b0794eb9fbed8bf3694adf7733cc177734
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-05-19 00:41:15 -0300

    3:23:01 Retornando o blog com as informações dentro do input

--- frontend/src/components/BlogDetail.jsx
+++ frontend/src/components/BlogDetail.jsx
@@ -1,12 +1,27 @@
 //frontend/src/component/blogdetail
+import { Box, Button, InputLabel, TextField, Typography } from '@mui/material'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
+const labelStyles = { mb: 1, mt: 2, fontSize: '24px', fontWeight: 'bold' }
 export const BlogDetail = () => {
   const [blog, setBlog] = useState()
   const id = useParams().id
   console.log(id)
+
+  const [inputs, setInputs] = useState({
+    title: '',
+    description: '',
+    imageURL: ''
+  })
+
+  const handleChange = e => {
+    setInputs(prevState => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }))
+  }
 
   const fetchDetails = async () => {
     const res = await axios
@@ -17,9 +32,84 @@ export const BlogDetail = () => {
   }
 
   useEffect(() => {
-    fetchDetails().then(data => setBlog(data))
+    fetchDetails().then(data => {
+      setBlog(data.blog)
+      setInputs({
+        title: data.blog.title,
+        description: data.blog.description,
+        // imageURL: data.blog.image  NAO SEI PORQUE MAIS SÓ FUNCIONOU COM O DE BAIXO
+        image: data.blog.image
+      })
+    })
   }, [id])
-  console.log(blog);
+  console.log(blog)
 
-  return <h1>BlogDetail</h1>
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log(inputs)
+    // sendRequest().then(data => console.log(data))
+  }
+
+  return (
+    <div>
+      {inputs && (
+        <form onSubmit={handleSubmit}>
+          <Box
+            boder={3}
+            borderColor="#6495ed"
+            borderRadius={10}
+            boxShadow="10px 10px 20px #ccc"
+            padding={3}
+            margin={'auto'}
+            marginTop={3}
+            display="flex"
+            flexDirection={'column'}
+            width={'80%'}
+          >
+            <Typography
+              fontWeight={'bold'}
+              padding={3}
+              color="grey"
+              variant="h2"
+              textAlign={'center'}
+            >
+              Post your Blog
+            </Typography>
+            <InputLabel sx={labelStyles}>Title</InputLabel>
+            <TextField
+              name="title"
+              onChange={handleChange}
+              value={inputs.title}
+              margin="normal"
+              variant="outlined"
+            />
+            <InputLabel sx={labelStyles}>Description</InputLabel>
+            <TextField
+              name="description"
+              onChange={handleChange}
+              value={inputs.description}
+              margin="normal"
+              variant="outlined"
+            />
+            <InputLabel sx={labelStyles}>ImageURL</InputLabel>
+            <TextField
+              name="imageURL"
+              onChange={handleChange}
+              value={inputs.image}
+              margin="normal"
+              variant="outlined"
+            />
+            <Button
+              sx={{ mt: 2, borderRadius: 4 }}
+              variant="contained"
+              color="warning"
+              type="submit"
+            >
+              Submit
+            </Button>
+          </Box>
+        </form>
+      )}
+    </div>
+  )
 }
