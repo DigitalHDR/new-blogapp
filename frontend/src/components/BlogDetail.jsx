0fb88a9b7e115d83fd6935b0980bbaa8c0d9ded9
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-05-19 00:04:38 -0300

    3:19:43 mostrando no console o blog que esta sendo recebido

--- frontend/src/components/BlogDetail.jsx
+++ frontend/src/components/BlogDetail.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+//frontend/src/component/blogdetail
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 export const BlogDetail = () => {
-  return (
-    <h1>
-      BlogDetail
-    </h1>
-  )
+  const [blog, setBlog] = useState()
+  const id = useParams().id
+  console.log(id)
+
+  const fetchDetails = async () => {
+    const res = await axios
+      .get(`http://localhost:5000/api/blog/${id}`)
+      .catch(err => console.log(err))
+    const data = await res.data
+    return data
+  }
+
+  useEffect(() => {
+    fetchDetails().then(data => setBlog(data))
+  }, [id])
+  console.log(blog);
+
+  return <h1>BlogDetail</h1>
 }
