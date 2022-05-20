de2c159137f4c1509326e5857c52857e2e4c4f84
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-05-20 19:19:40 -0300

    3:46:00 após atualizar a pagina estando logado, vai permanecer logado

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,25 @@
 //frontend/src/app.jsx
 import { Header } from './components/Header'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { Auth } from './components/Auth'
 import { Blogs } from './components/Blogs'
 import { UserBlogs } from './components/UserBlogs'
 import { BlogDetail } from './components/BlogDetail'
 import { AddBlog } from './components/AddBlog'
-import { useSelector } from 'react-redux'
+
+import { useDispatch, useSelector } from 'react-redux'
+import { authActions } from './store'
 
 function App() {
+  const dispath = useDispatch()
   const isLoggedIn = useSelector(state => state.isLoggedIn)
+
+  useEffect(() => {
+    if (localStorage.getItem('userId')) {
+      dispath(authActions.login())
+    }
+  }, [dispath])
 
   return (
     <React.Fragment>
