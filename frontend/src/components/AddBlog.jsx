0f8f7592f8a14d4d0fa4be8ec4b7f42aa904a032
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-05-20 01:02:07 -0300

    3:34:24 após criar um posta volta para pagina blogs

--- frontend/src/components/AddBlog.jsx
+++ frontend/src/components/AddBlog.jsx
@@ -2,9 +2,11 @@
 import React, { useState } from 'react'
 import { Box, Button, InputLabel, TextField, Typography } from '@mui/material'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const labelStyles = { mb: 1, mt: 2, fontSize: '24px', fontWeight: 'bold' }
 export const AddBlog = () => {
+  const navigate = useNavigate()
   const [inputs, setInputs] = useState({
     title: '',
     description: '',
@@ -34,7 +36,9 @@ export const AddBlog = () => {
   const handleSubmit = e => {
     e.preventDefault()
     console.log(inputs)
-    sendRequest().then(data => console.log(data))
+    sendRequest()
+      .then(data => console.log(data))
+      .then(() => navigate('/blogs'))
   }
 
   return (
