96cab0bd55fe4d29b2487d920769b3130e873a7a
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-05-23 17:14:22 -0300

    1 parte do front ok

---
+++ frontend/server.js
@@ -0,0 +1,13 @@
+//frontend/server.js
+const express = require('express')
+const { resolve } = require('path')
+const app = express()
+
+app.use('/', express.static(resolve(__dirname, './build')))
+
+app.listen(process.env.PORT || 3000, err => {
+  if (err) {
+    return console.log(err)
+  }
+  console.log('tudo funcionandoooooooooooooo')
+})
